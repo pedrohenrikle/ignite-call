8b6da58601e98a7a84fde38d04a66288008d5963
pedrohenrikle <pedro.klein.sl@gmail.com>
2023-04-30 09:56:34 -0300

    ci: Added home and register pages

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,33 @@
+import { Heading, Text } from '@ignite-ui/react'
+import { Container, Hero, Preview } from './styles'
+
+import Image from 'next/image'
+
+import previewImage from '../../assets/app-preview.png'
+import { ClaimUsernameForm } from './components/ClaimUsernameForm'
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading size={'4xl'}>Agendamento Descomplicado</Heading>
+        <Text size={'lg'}>
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+
+        <ClaimUsernameForm />
+      </Hero>
+
+      <Preview>
+        <Image
+          src={previewImage}
+          height={400}
+          quality={100}
+          priority
+          alt="Calendário simbolizando aplicação em funcionamento"
+        />
+      </Preview>
+    </Container>
+  )
+}
---
+++ src/pages/register/index.page.tsx
@@ -0,0 +1,87 @@
+import { ArrowRight } from 'phosphor-react'
+import { Container, Form, FormError, Header } from './styles'
+import { Heading, Text, MultiStep, TextInput, Button } from '@ignite-ui/react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+
+const registerFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: 'O usuário precisa ter no mínimo 3 letras' })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: 'O usuário pode ter apenas letras e hifens ',
+    })
+    .transform((username) => username.toLowerCase()),
+
+  name: z
+    .string()
+    .min(3, { message: 'O nome precisa ter pelo menos 3 letras' }),
+})
+
+type RegisterFormData = z.infer<typeof registerFormSchema>
+
+export default function Register() {
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors, isSubmitting },
+  } = useForm<RegisterFormData>({
+    resolver: zodResolver(registerFormSchema),
+  })
+
+  const router = useRouter()
+
+  useEffect(() => {
+    if (router.query.username) {
+      setValue('username', String(router.query.username))
+    }
+  }, [router.query?.username, setValue])
+
+  async function handleRegister(data: RegisterFormData) {}
+
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Bem-vindo ao Ignite Call</Heading>
+        <Text>
+          Precisamos de algumas informações para criar seu perfil! Ah, você pode
+          editar essas informações depois.
+        </Text>
+
+        <MultiStep currentStep={1} size={4} />
+
+        <Form as="form" onSubmit={handleSubmit(handleRegister)}>
+          <label>
+            <Text size={'sm'}>Nome de usuário</Text>
+            <TextInput
+              prefix="ignite.com/"
+              placeholder="seu-usuario"
+              {...register('username')}
+            />
+
+            {errors.username && (
+              <FormError size={'sm'}>{errors.username.message}</FormError>
+            )}
+          </label>
+          <label>
+            <Text size={'sm'}>Nome completo</Text>
+            <TextInput placeholder="Seu nome" {...register('name')} />
+
+            {errors.name && (
+              <FormError size={'sm'}>{errors.name.message}</FormError>
+            )}
+          </label>
+
+          <Button type="submit" disabled={isSubmitting}>
+            Próximo passo
+            <ArrowRight />
+          </Button>
+        </Form>
+      </Header>
+    </Container>
+  )
+}
